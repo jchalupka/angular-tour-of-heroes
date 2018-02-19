76b48f867381feb2b3030a00f262c1ecdda6f426
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2018-02-18 19:13:46 -0500

    adding step 4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+console.log('HEROES');
+console.log(HEROES);
 
 @Component({
   selector: 'app-heroes',
@@ -7,14 +11,21 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero = {
-    id: 1,
-    name: 'Windstorm'
+  heroes = HEROES;
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    if(hero === this.selectedHero) {
+      this.selectedHero = undefined;
+    }
+    else {
+      this.selectedHero = hero;
+    }
   }
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
